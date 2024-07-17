3463eb682e805f4a6670e1273bde11d929d40d5c
Rui Martins <rui_pedru@hotmail.com>
2024-07-17 19:38:44 +0100

    cenas

--- hello.c
+++ hello.c
@@ -5,8 +5,5 @@ int main (void)
 	printf("hello git!\n");
 	printf("another test\n");
 	printf("print from branch\n");
-
-	printf("commit pela  main");
-	printf("commimt pelo testde");
 	return 0;
 }
