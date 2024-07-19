5f3303841af31677776584615b14389f2f7db1f2
Rui Martins <rui_pedru@hotmail.com>
2024-07-19 18:23:24 +0100

    upload minishell

---
+++ minishell/includes/minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/19 17:34:50 by ruidos-s          #+#    #+#             */
+/*   Updated: 2024/07/19 18:07:03 by ruidos-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/libft.h"
+# include <errno.h>
+# include <sys/wait.h>
+
+
+
+#endif
---
+++ minishell/libft/ft_printf.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 15:36:27 by ruidos-s          #+#    #+#             */
+/*   Updated: 2024/04/18 14:44:03 by ruidos-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	int		count;
+
+	if (!format)
+		return (-1);
+	va_start(ap, format);
+	count = 0;
+	while (*format)
+	{
+		if (*format == '%' && !*(format + 1))
+		{
+			return (-1);
+			va_end(ap);
+		}
+		if (*format == '%')
+			count += print_format(*(++format), ap);
+		else
+			count += write(1, format, 1);
+		format++;
+	}
+	va_end(ap);
+	return (count);
+}
