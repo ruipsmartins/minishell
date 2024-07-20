e95adad5552c0d524c6a1cdc2e9cf39b4481e00a
Rui Martins <rui_pedru@hotmail.com>
2024-07-20 14:06:52 +0100

    aprender readline

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 17:34:50 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/07/19 18:07:03 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/07/20 14:02:39 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@
 # include "../libft/libft.h"
 # include <errno.h>
 # include <sys/wait.h>
+# include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
+char *readline (const char *prompt);
 
 
 #endif
--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,14 +6,30 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 17:28:57 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/07/19 18:11:27 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/07/20 14:02:33 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main (void)
-{
-	ft_printf("teste\n");
-	return (0);
+
+
+int main() {
+    // Define um prompt para o utilizador
+    const char* prompt = "Introduz uma linha: ";
+
+    // Usa readline para obter uma linha de input do utilizador
+    char* input = readline(prompt);
+
+    if (input != NULL) {
+        // A linha de input é retornada alocada com malloc, então precisamos libertá-la depois de a usar
+        printf("Leste: %s\n", input);
+        free(input);
+    } else {
+        printf("Erro ao ler a linha ou linha vazia.\n");
+    }
+
+    return 0;
 }
