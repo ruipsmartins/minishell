0d082a24a3c440570390e1cbb300d3724510214f
Addicted <addicted@pop-os.lan>
2024-09-17 10:51:46 +0100

    main

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: addicted <addicted@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 17:28:57 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/09/10 10:44:34 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/09/17 10:51:17 by addicted         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ int	main(int ac, char **av, char **env)
 		input = get_command_input();
 	}
 	free(input);
+	printf("test\n");
 	rl_clear_history();
 	return (0);
 }
