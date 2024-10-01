eaf9b8110c1369464af41a5c6b8b247ab54a948f
Rui Martins <rui_pedru@hotmail.com>
2024-10-01 12:50:50 +0100

    "Updated Makefile to enable compiler warnings and flags; updated timestamps in execute.c and pipes.c; refactored pipes.c to improve pipe handling and error handling in execute_piped_commands function."

--- src/pipes.c
+++ src/pipes.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 11:05:58 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/10/01 11:26:25 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/10/01 12:42:45 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ void execute_piped_commands(t_command *cmd, char **env)
 {
 	int		fd[2];
 	pid_t	pid;
-	int		i = 0;
-	int		in_fd = 0;
+	int		in_fd = 0; // Ficheiro de entrada do comando anterior
 
 	while (cmd != NULL)
 	{
@@ -25,23 +24,41 @@ void execute_piped_commands(t_command *cmd, char **env)
 		pid = fork();
 		if (pid == 0)
 		{
-			dup2(in_fd, STDIN_FILENO); // Entrada do pipe anterior
-			if (cmd->next)
+			// No processo filho
+			if (in_fd != 0)
+			{
+				dup2(in_fd, STDIN_FILENO); // Entrada do pipe anterior
+				close(in_fd);
+			}
+			if (cmd->next != NULL)
+			{
 				dup2(fd[1], STDOUT_FILENO); // Saída para o próximo pipe
-			close_fds(fd);
-			handle_redirects(cmd); // Tratar redirecionamentos dentro de cada comando
-			execute(cmd, env);
-			//perror("execve error");
+				close(fd[0]); // Fechar a leitura do pipe atual no processo filho
+			}
+			close(fd[1]); // Fechar a escrita do pipe
+
+			// Tratar redirecionamentos se necessário
+			/* if (handle_redirects(cmd, NULL, NULL) == -1)
+				exit(EXIT_FAILURE); */
+
+			// Executar o comando
+			execute_command(cmd->args[0], cmd->args, env);
+
+			perror("execve error"); // Se o execve falhar
 			exit(EXIT_FAILURE);
 		}
 		else if (pid < 0)
+		{
 			perror("fork error");
+			exit(EXIT_FAILURE);
+		}
 		else
 		{
-			wait(NULL);
-			close(fd[1]);
-			in_fd = fd[0]; // Guardar a entrada para o próximo comando
-			i++;
+			// No processo pai
+			wait(NULL); // Esperar que o processo filho termine
+			close(fd[1]); // Fechar o descritor de escrita no pai
+			in_fd = fd[0]; // Guardar a leitura para o próximo comando
+			cmd = cmd->next; // Avançar para o próximo comando na pipeline
 		}
 	}
 }
