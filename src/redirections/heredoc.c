57319f6e83e05fb6d03f08aaf63936e1b232a2ad
Rui Martins <rui_pedru@hotmail.com>
2024-10-16 18:36:15 +0100

    "Updated heredoc.c and outfile with minor changes"

--- src/redirections/heredoc.c
+++ src/redirections/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 14:01:43 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/10/16 18:10:13 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/10/16 18:24:37 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int	execute_heredoc(t_command *cmd)
 			free(line);
 			break ;
 		}
-		write(pipe_fd[1], line, strlen(line));
+		write(pipe_fd[1], line, ft_strlen(line));
 		write(pipe_fd[1], "\n", 1);
 		free(line);
 	}
