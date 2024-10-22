c956d70b423e46cb0157a1a70368f024ac020ce8
Rui Martins <rui_pedru@hotmail.com>
2024-10-22 17:03:13 +0100

    exit_command a funcionar

--- src/pipes/pipes.c
+++ src/pipes/pipes.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 11:05:58 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/10/22 12:37:31 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/10/22 16:23:08 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,45 +29,6 @@ void	handle_fd(int in_fd, t_command *cmd, int fd[2])
 }
 
 // Função para executar o comando no processo filho.
-/* void	ft_child(int in_fd, t_command *cmd, int fd[2], t_data *data)
-{
-	int		fd[2];
-	pid_t	pid;
-	int		in_fd;
-
-	if (handle_redirects(cmd, data) == -1)
-		exit(EXIT_FAILURE);
-	handle_fd(in_fd, cmd, fd);
-	execute_command_or_path(cmd, data);
-	std_reset(&data->original_stdin, &data->original_stdout);
-	exit(EXIT_FAILURE);
-} */
-/* Função para executar comandos em sequência,
-utilizando pipes para a comunicação entre processos. */
-/* void	execute_piped_commands(t_command *cmd, t_data *data)
-{
-	in_fd = 0;
-	while (cmd != NULL)
-	{
-		pipe(fd);
-		pid = fork();
-		if (pid == 0)
-			ft_child(in_fd, cmd, fd, data);
-		else if (pid < 0)
-		{
-			perror("fork error:");
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			wait(NULL);
-			close(fd[1]);
-			in_fd = fd[0];
-			cmd = cmd->next;
-		}
-	}
-} */
-//////////////////////////////////////////////////////////////////////////
 int	ft_child(int in_fd, t_command *cmd, int fd[2], t_data *data,
 		int exit_pipe[2])
 {
@@ -90,6 +51,8 @@ int	ft_child(int in_fd, t_command *cmd, int fd[2], t_data *data,
 	exit(EXIT_SUCCESS);  // Sai do processo filho
 }
 
+/* Função para executar comandos em sequência,
+utilizando pipes para a comunicação entre processos. */
 void	execute_piped_commands(t_command *cmd, t_data *data)
 {
 	pid_t	pid;
@@ -104,20 +67,13 @@ void	execute_piped_commands(t_command *cmd, t_data *data)
 		pipe(fd);        // Cria o pipe para comunicação entre comandos
 		pipe(exit_pipe); // Cria o pipe para comunicação de 'exit'
 		pid = fork();
-		if (pid == 0)
-		{
-			close(exit_pipe[0]);
-			// Fecha o lado de leitura no filho
-			ft_child(in_fd, cmd, fd, data, exit_pipe);
-			// Passa o novo pipe para ft_child
-			exit(EXIT_SUCCESS);
-			// Sai do filho após executar o comando
-		}
-		else if (pid < 0)
+		if (pid < 0)
 		{
 			perror("fork error:");
 			exit(EXIT_FAILURE);
 		}
+		else if (pid == 0)
+			ft_child(in_fd, cmd, fd, data, exit_pipe);
 		else
 		{
 			close(fd[1]);        // Fecha a extremidade de escrita do pipe
