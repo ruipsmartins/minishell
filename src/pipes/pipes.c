208d97213cad0cd3625487d7fa5a57afb0058267
Rui Martins <rui_pedru@hotmail.com>
2024-10-25 11:49:15 +0100

    criação ft_parent

--- src/pipes/pipes.c
+++ src/pipes/pipes.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 11:05:58 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/10/22 16:23:08 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/10/25 11:23:28 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	handle_fd(int in_fd, t_command *cmd, int fd[2])
 int	ft_child(int in_fd, t_command *cmd, int fd[2], t_data *data,
 		int exit_pipe[2])
 {
-	close(exit_pipe[0]); // Fecha o lado de leitura no filho
+	close(exit_pipe[0]);
 	if (ft_strncmp(cmd->args[0], "exit", 5))
 	{
 		if (handle_redirects(cmd, data) == -1)
@@ -41,14 +41,27 @@ int	ft_child(int in_fd, t_command *cmd, int fd[2], t_data *data,
 	}
 	execute_command_or_path(cmd, data);
 	std_reset(&data->original_stdin, &data->original_stdout);
-	// Se o comando for 'exit', envia um sinal para o pai.
 	if (data->close_shell)
 	{
 		write(exit_pipe[1], "1", 1);
-		// Escreve '1' para indicar que o comando 'exit' foi encontrado.
 	}
-	close(exit_pipe[1]); // Fecha o pipe de comunicação
-	exit(EXIT_SUCCESS);  // Sai do processo filho
+	close(exit_pipe[1]);
+	exit(EXIT_SUCCESS);
+}
+
+bool	ft_parent(int *in_fd, int fd[2], t_data *data, int exit_pipe[2])
+{
+	char	exit_signal;
+
+	close(fd[1]); // Fecha a extremidade de escrita do pipe
+	close(exit_pipe[1]);
+	if (read(exit_pipe[0], &exit_signal, 1) > 0 && exit_signal == '1')
+	{
+		data->close_shell = true;
+		return (true);
+	}
+	*in_fd = fd[0]; // Prepara para o próximo comando
+	return (false);
 }
 
 /* Função para executar comandos em sequência,
@@ -57,15 +70,14 @@ void	execute_piped_commands(t_command *cmd, t_data *data)
 {
 	pid_t	pid;
 	int		in_fd;
-	char	exit_signal;
+	int		fd[2];
+	int		exit_pipe[2];
 
-	int fd[2];        // Pipe para comunicação entre comandos
-	int exit_pipe[2]; // Novo pipe para comunicar 'exit'
 	in_fd = 0;
 	while (cmd != NULL)
 	{
-		pipe(fd);        // Cria o pipe para comunicação entre comandos
-		pipe(exit_pipe); // Cria o pipe para comunicação de 'exit'
+		pipe(fd);
+		pipe(exit_pipe);
 		pid = fork();
 		if (pid < 0)
 		{
@@ -76,16 +88,8 @@ void	execute_piped_commands(t_command *cmd, t_data *data)
 			ft_child(in_fd, cmd, fd, data, exit_pipe);
 		else
 		{
-			close(fd[1]);        // Fecha a extremidade de escrita do pipe
-			close(exit_pipe[1]); // Fecha a extremidade de escrita do exit pipe
-			wait(NULL);          // Espera pelo filho
-			// Lê do exit_pipe para verificar se o comando 'exit' foi encontrado
-			if (read(exit_pipe[0], &exit_signal, 1) > 0 && exit_signal == '1')
-			{
-				data->close_shell = true; // Define a flag para fechar o shell
-				break ;
-			}
-			in_fd = fd[0];   // Prepara para o próximo comando
+			if (ft_parent(&in_fd, fd, data, exit_pipe))
+				break ;       // Sai do loop se 'exit' foi encontrado
 			cmd = cmd->next; // Avança para o próximo comando
 		}
 	}
